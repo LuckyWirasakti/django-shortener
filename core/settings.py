de09126e97710596868f1edf3195f7dda85ead49
Lucky Wirasakti <lucky.wirasakti@gmail.com>
2021-03-13 21:29:23 +0700

    initial commit

--- core/settings.py
+++ core/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = DEFAULT_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/core/static/',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
